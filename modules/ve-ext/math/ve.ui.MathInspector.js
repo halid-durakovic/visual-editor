cbb457924ea7c35f67c10a4c905e3e6e84bc36fe
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-06-12 22:04:54 +0200

    Create a new math node when used via toolbar.

--- modules/ve-ext/math/ve.ui.MathInspector.js
+++ modules/ve-ext/math/ve.ui.MathInspector.js
@@ -15,8 +15,8 @@
  * @param {Object} [config] Configuration options
  */
 ve.ui.MathInspector = function VeUiMathInspector( config ) {
-  // Parent constructor
-  ve.ui.Inspector.call( this, config );
+	// Parent constructor
+	ve.ui.Inspector.call( this, config );
 };
 
 /* Inheritance */
@@ -41,93 +41,114 @@ ve.ui.MathInspector.static.modelClasses = [ ve.dm.MathNode ];
  * @inheritdoc
  */
 ve.ui.MathInspector.prototype.getInsertionText = function () {
-  console.log("ve.ui.MathInspector.getInsertionText()");
-  return this.mathInput.getValue();
+	console.log("ve.ui.MathInspector.getInsertionText()");
+	return this.mathInput.getValue();
 };
 
 /**
  * @inheritdoc
  */
 ve.ui.MathInspector.prototype.initialize = function () {
-  console.log("ve.ui.MathInspector.initialize()");
+	console.log("ve.ui.MathInspector.initialize()");
 
-  // Parent method
-  ve.ui.MathInspector.super.prototype.initialize.call( this );
+	// Parent method
+	ve.ui.MathInspector.super.prototype.initialize.call( this );
 
-  // Properties
-  this.mathInput = new this.constructor.static.mathInputWidget(this.node, {
-    '$': this.$, '$overlay': this.$contextOverlay || this.$overlay
-  } );
+	// Properties
+	this.mathInput = new this.constructor.static.mathInputWidget(this.node, {
+		'$': this.$, '$overlay': this.$contextOverlay || this.$overlay
+	} );
 
-  this.mathInput.connect( this, {'change': 'onFormulaChange'} );
+	this.mathInput.connect( this, {'change': 'onFormulaChange'} );
 
 
-  // Initialization
-  this.$form.append( this.mathInput.$element );
+	// Initialization
+	this.$form.append( this.mathInput.$element );
 };
 
 /**
  * @inheritdoc
  */
 ve.ui.MathInspector.prototype.getSetupProcess = function ( data ) {
-  return ve.ui.MathInspector.super.prototype.getSetupProcess.call( this, data )
-    .next( function () {
-      this.$contextOverlay.addClass("math-inspector");
+	return ve.ui.MathInspector.super.prototype.getSetupProcess.call( this, data )
+		.next( function () {
+			this.$contextOverlay.addClass("math-inspector");
 
-      console.log("ve.ui.MathInspector.getReadyProcess()");
-      var fragment = this.getFragment();
-      if (!fragment) {
-        return false;
-      }
-      this.node = fragment.getSelectedNode();
-      if (!this.node) {
-        return false;
-      }
-      this.mathInput.$input.val(this.node.getFormula());
-    }, this );
+			console.log("ve.ui.MathInspector.getReadyProcess()");
+			var fragment = this.getFragment();
+			if (!fragment) {
+				return false;
+			}
+			this.node = fragment.getSelectedNode();
+
+			if (!this.node) {
+				var type;
+				var leafNodes = fragment.getLeafNodes();
+				if (!leafNodes.length) {
+					return false;
+				}
+				if (leafNodes[0].node.parent === leafNodes[0].node.root) {
+					type = "mathBlock";
+				} else {
+					type = "mathInline";
+				}
+				fragment.insertContent([
+					{
+						type: type,
+						attributes: {
+							formula: ""
+						}
+					}
+				], false ).collapseRangeToEnd().select();
+				this.node = fragment.getSelectedNode();
+				fragment.select(this.node.getOuterRange());
+			}
+
+			this.mathInput.$input.val(this.node.getFormula());
+		}, this );
 };
 
 /**
  * @inheritdoc
  */
 ve.ui.MathInspector.prototype.getReadyProcess = function (data) {
-  return ve.ui.MathInspector.super.prototype.getReadyProcess.call( this )
-    .next( function () {
-      this.mathInput.focus();
-    }, this );
+	return ve.ui.MathInspector.super.prototype.getReadyProcess.call( this )
+		.next( function () {
+			this.mathInput.focus();
+		}, this );
 };
 
 /**
  * @inheritdoc
  */
 ve.ui.MathInspector.prototype.getTeardownProcess = function ( data ) {
-  return ve.ui.MathInspector.super.prototype.getTeardownProcess.call( this, data )
-    .first( function () {
-      console.log("ve.ui.MathInspector.getTeardownProcess()", data);
-      this.$contextOverlay.removeClass("math-inspector");
-      // Configuration initialization
-      data = data || {};
-    }, this);
+	return ve.ui.MathInspector.super.prototype.getTeardownProcess.call( this, data )
+		.first( function () {
+			console.log("ve.ui.MathInspector.getTeardownProcess()", data);
+			this.$contextOverlay.removeClass("math-inspector");
+			// Configuration initialization
+			data = data || {};
+		}, this);
 };
 
 ve.ui.MathInspector.prototype.onFormulaChange = function() {
-  var newFormula = this.mathInput.getValue();
-  var fragment = this.getFragment();
-  if (fragment) {
-    fragment.changeAttributes(
-      {
-        'formula': newFormula
-      },
-      this.node.getType()
-    );
-  }
+	var newFormula = this.mathInput.getValue();
+	var fragment = this.getFragment();
+	if (fragment) {
+		fragment.changeAttributes(
+			{
+				'formula': newFormula
+			},
+			this.node.getType()
+		);
+	}
 };
 
 /**
  * @inheritdoc
  */
 ve.ui.MathInspector.prototype.isEmbeddable = function() {
-  return false;
+	return false;
 }
 
 
