5f2cf56ef18f58eff9b08278ba85b50e04d427ab
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-10-08 23:57:52 +0200

    Cache last crossref query result.

--- ext/ve.ui.CrossrefCitationLookup.js
+++ ext/ve.ui.CrossrefCitationLookup.js
@@ -1,5 +1,7 @@
 
 ve.ui.CrossrefCitationLookup = function VeUiCrossrefCitationLookup() {
+  this.lastSearch = "";
+  this.lastResult = [];
 };
 
 OO.inheritClass( ve.ui.CrossrefCitationLookup, ve.ui.CitationLookupService );
@@ -31,37 +33,53 @@ ve.ui.CrossrefCitationLookup.resolveDOI = function( doi ) {
 
 ve.ui.CrossrefCitationLookup.prototype.find = function( searchStr, context ) {
   var promisedResult = $.Deferred();
-  searchStr = "Bishop 1992";
 
-  var searchTerms = searchStr.trim().toLowerCase().split(/\s+/);
-  var doiQueryParams = searchTerms.join('+');
-  var count = 0;
-
-  $.ajax('http://search.crossref.org/dois', {
-    data: {
-      q: doiQueryParams,
-      sort: "score"
-    },
-    success: function(searchResult) {
-      searchResult.forEach(function(entry) {
-        var promisedData = ve.ui.CrossrefCitationLookup.resolveDOI(entry.doi);
-        promisedData.done(function(data) {
-          window.console.log("Progressing: ", data);
-          promisedResult.notifyWith(context, [ data ]);
-        });
-        promisedData.always(function() {
-          count++;
-          if (count === searchResult.length) {
-            promisedResult.resolveWith(context);
-          }
-        });
+  // deliver a cached result if the search has not changed
+  if (this.lastSearch === searchStr) {
+    var lastResult = this.lastResult;
+    window.setTimeout(function() {
+      lastResult.forEach(function(data) {
+        promisedResult.notifyWith(context, [ data ]);
       });
-    },
-    error: function(req, status, err) {
-      promisedResult.rejectWith(context, [err]);
-    }
-  });
+      promisedResult.resolveWith(context);
+    }, 0);
+  }
+  // otherwise start a new search;
+  else {
+    this.lastSearch = searchStr;
+    var lastResult = [];
+    this.lastResult = lastResult;
 
+    var searchTerms = searchStr.trim().toLowerCase().split(/\s+/);
+    var doiQueryParams = searchTerms.join('+');
+    var count = 0;
+
+    $.ajax('http://search.crossref.org/dois', {
+      data: {
+        q: doiQueryParams,
+        sort: "score"
+      },
+      success: function(searchResult) {
+        searchResult.forEach(function(entry) {
+          var promisedData = ve.ui.CrossrefCitationLookup.resolveDOI(entry.doi);
+          promisedData.done(function(data) {
+            window.console.log("Progressing: ", data);
+            lastResult.push(data);
+            promisedResult.notifyWith(context, [ data ]);
+          });
+          promisedData.always(function() {
+            count++;
+            if (count === searchResult.length) {
+              promisedResult.resolveWith(context);
+            }
+          });
+        });
+      },
+      error: function(req, status, err) {
+        promisedResult.rejectWith(context, [err]);
+      }
+    });
+  }
   return promisedResult;
 };
 
